01acacdd8f5fa867b811bd276531d53008d92c3c
Dylan Roussel <mail@evowizz.dev>
2022-05-31 13:53:31 +0200

    Add AppBar, MainFAB & PaletteButton components

---
+++ app/src/main/java/com/rswiftkey/ui/components/RwiftkeyAppBar.kt
@@ -0,0 +1,33 @@
+package com.rswiftkey.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.rswiftkey.R
+
+@Composable
+fun RwiftkeyAppBar(
+    modifier: Modifier = Modifier,
+    title: String = stringResource(id = R.string.app_name),
+    showSettings: Boolean = false,
+    onSettingsClick: () -> Unit = { }
+) = LargeTopAppBar(
+    modifier = modifier,
+    title = { Text(text = title) },
+    actions = {
+        if (showSettings) {
+            IconButton(onClick = onSettingsClick) {
+                Icon(
+                    imageVector = Icons.Outlined.Settings,
+                    contentDescription = "Settings",
+                )
+            }
+        }
+    }
+)
---
+++ app/src/main/java/com/rswiftkey/ui/components/RwiftkeyPaletteButton.kt
@@ -0,0 +1,45 @@
+package com.rswiftkey.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Palette
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.rswiftkey.R
+
+@Composable
+fun RwiftkeyPaletteButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.onSecondary,
+        shape = MaterialTheme.shapes.extraLarge,
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(50.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Palette,
+                contentDescription = "Palette",
+                modifier = Modifier.size(96.dp)
+            )
+            Text(text = stringResource(R.string.open_theme_section))
+        }
+    }
+
+}
