16167d6fd3029c0ce730bd4afee7b23357afc1c9
Dylan Roussel <mail@evowizz.dev>
2022-05-31 14:15:22 +0200

    Prepare for Hilt integration

--- app/src/main/java/com/rswiftkey/MainActivityV2.kt
+++ app/src/main/java/com/rswiftkey/MainActivityV2.kt
@@ -5,8 +5,10 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.rswiftkey.ui.RwiftkeyApp
+import dagger.hilt.android.AndroidEntryPoint
 
 // TODO: Rename to MainActivity once ready to be released
+@AndroidEntryPoint
 class MainActivityV2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
