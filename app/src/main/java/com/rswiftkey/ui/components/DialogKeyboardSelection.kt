0bde3448a14a7f2885bc3fbed0d0e234ea9ad56b
VegaBobo <vegazstestando@gmail.com>
2022-06-04 00:43:53 -0300

    Add dialog keyboard selection composable forgot to add it on last commit

---
+++ app/src/main/java/com/rswiftkey/ui/components/DialogKeyboardSelection.kt
@@ -0,0 +1,36 @@
+package com.rswiftkey.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.rswiftkey.R
+import com.rswiftkey.SimpleApplication
+
+@Composable
+fun DialogKeyboardSelection(
+    availKeyboards: List<SimpleApplication>,
+    onDismissRequest: () -> Unit,
+    onClick: (SimpleApplication) -> Unit,
+) {
+    AlertDialog(
+        modifier = Modifier.fillMaxWidth(),
+        onDismissRequest = onDismissRequest,
+        title = { Text(text = stringResource(id = R.string.select_keyboard)) },
+        confirmButton = {
+            Column {
+                for (t in availKeyboards) {
+                    PreferenceItem(
+                        title = t.applicationName,
+                        description = t.packageName,
+                        icon = null,
+                        onClick = { onClick(t) }
+                    )
+                }
+            }
+        }
+    )
+}
