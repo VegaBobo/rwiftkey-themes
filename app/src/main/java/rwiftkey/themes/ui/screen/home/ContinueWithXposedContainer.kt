abf6512023a65ced323f560469aa066f870ea43c
VegaBobo <vegazstestando@gmail.com>
2023-06-02 22:39:05 -0300

    Replace divider with spaces

--- app/src/main/java/rwiftkey/themes/ui/screen/home/ContinueWithXposedContainer.kt
+++ app/src/main/java/rwiftkey/themes/ui/screen/home/ContinueWithXposedContainer.kt
@@ -4,18 +4,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -44,7 +43,7 @@ fun ContinueWithXposedContainer(
                 text = stringResource(id = R.string.dialog_no_root_desc),
                 textAlign = TextAlign.Center
             )
-            Divider(modifier = Modifier.padding(12.dp), color = Color.Black, thickness = 1.dp)
+            Spacer(modifier = Modifier.padding(4.dp))
             Text(
                 modifier = Modifier.padding(4.dp),
                 fontWeight = FontWeight.Medium,
@@ -55,6 +54,7 @@ fun ContinueWithXposedContainer(
                 text = stringResource(id = R.string.proceed_without_root_desc),
                 textAlign = TextAlign.Center
             )
+            Spacer(modifier = Modifier.padding(2.dp))
             TextButton(onClick = { onClickContinue() }) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Text(text = stringResource(id = R.string.mcontinue))
@@ -65,6 +65,7 @@ fun ContinueWithXposedContainer(
                     )
                 }
             }
+            Spacer(modifier = Modifier.padding(2.dp))
             Text(
                 text = stringResource(id = R.string.continue_desc),
                 textAlign = TextAlign.Center
